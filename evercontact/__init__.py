7b19181d5ce75143f07f82a9ecd46d5edb03e9f4
Ronnie Beltran <rbbeltran.09@gmail.com>
2015-03-23 11:40:47 +0800

    Update fetch docstring

--- evercontact/__init__.py
+++ evercontact/__init__.py
@@ -17,7 +17,34 @@ class EverContact(object):
     def fetch(self, payload, signature_format='json'):
         """ EverContact API by default returns signature data in VCard format.
         signature: String
-        Can be either in `vcard` or `json`
+            Can be either in `vcard` or `json`
+        payload: dict
+            Mandatory keys are the following:
+            - ApiUser: API login access.
+            - Date: The Message Date, UTC and ISO formatted.
+                (e.g. 2011-11-04 13:44:34)
+            - Subject: The Message Subject
+            - HeaderFrom: The Message Sender(s).
+                Please note this parameter should be
+                repeated when there are multiple senders.
+            - HeaderTo: The Message “To” Recipient(s).
+                Please note this parameter should be
+                repeated when there are multiple receivers.
+            - AddressingMode: Specify whether the mail is an
+                outgoing (EXPLICIT_FROM) or incoming (EXPLICIT_TO,
+                EXPLICIT_CC).  If this information is not available,
+                please include OTHER.
+            - Content: The Message Content, plain text/html.
+                Note: Content having more than 100 000 bytes
+                will return an Error
+
+            Optional keys are the following:
+            - HeaderCC: The Message “Cc” or “Bcc” Recipients
+                Please note this parameter should be repeated
+                when there are multiple receivers.
+            - AttachedFiles: Use File Names & extension.
+                Please note this parameter should be repeated when there are
+                multiple file attachments.
         """
 
         resp = requests.post(EVERCONTACT_API_ENDPOINT, data=payload,
